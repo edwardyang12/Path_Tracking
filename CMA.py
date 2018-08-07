fixed the car's angle updates and waypoint calculation. (using distance-error-and-goal-penalty cost function)

CMA.py
@@ -16,13 +16,11 @@ from cma import fmin2
 
 
 
-N_TRAIN_DIRECTIONS = 24
+N_TRAIN_DIRECTIONS = 1
 N_TRAIN_TRAJ = 24
-N_TEST_DIRECTIONS = 6
 N_NEURONS = 10
 VELOCITY = 1
 STEP = 33
-N_TEST_TRAJ = 5
 
 def generate_path(STEP):
 	path_list = []
@@ -39,8 +37,7 @@ def generate_path(STEP):
 
 def generate_paths():
 	paths =[]
-	total = N_TEST_TRAJ + N_TRAIN_TRAJ
-	for i in range(total):
+	for i in range(N_TRAIN_TRAJ):
 		path_list = generate_path(STEP)
 		paths.append(path_list)
 	return paths
@@ -65,31 +62,32 @@ if __name__ == '__main__':
             controller = NN(x).controller
             trackers = []
             for i in range(N_TRAIN_DIRECTIONS):
-                for traj in paths[:N_TRAIN_TRAJ]:
+                for traj in paths:
                     trackers.append([x,v,i*2*pi/N_TRAIN_DIRECTIONS,traj])
             costs = pool.map(function_x, trackers)
-            #return reduce(lambda x, y: x+y, costs)/len(costs)
             return max(costs)
         x = 2*rand(1,4*N_NEURONS+1)[0]-1
         res = fmin2(objective,
                         x,
                         .5,
                         args=(VELOCITY, ),
-                        options={'popsize': 152,
+                        options={'popsize': 256,
                                 'bounds': [-1, 1],
-                                'maxiter': 200}) # 5th is mean of final sample distribution
+                                'maxiter': 256}) # 5th is mean of final sample distribution
         res=res[1].result[0]
         controller = NN(res).controller
         trackers = []
-        for i in range(N_TEST_DIRECTIONS):
-            for t in paths[N_TRAIN_TRAJ:]:
+        for i in range(N_TRAIN_DIRECTIONS):
+            for t in paths:
                 traj = Trajectory(t)
-                orientation = i*2*pi/N_TEST_DIRECTIONS
+                orientation = i*2*pi/N_TRAIN_DIRECTIONS
                 trackers.append(Tracker(VELOCITY,orientation,traj,controller))
         traces = map(lambda x: x.run(), trackers)
         for i, trace in enumerate(traces):
-            plt.plot(trace[1], trace[2], '-', color=hsv_to_rgb(linspace(0, 1, len(traces))[i],1,1))
-        plt.show()
+            plt.plot(trace[1], trace[2], ':', color=hsv_to_rgb(linspace(0, 1, len(traces))[i],1,1))
+	for path in paths:
+	    plt.plot(zip(*path))        
+	plt.show()
         res = res.tolist()
         f = open(sys.argv[1],"w+")
         f.write(str(res))
@@ -97,22 +95,21 @@ if __name__ == '__main__':
 
     elif sys.argv[1] == '-i' and len(sys.argv) == 3:
         if os.path.isfile(sys.argv[2]):
-            #traj_points = zip(*t)
-            #plt.plot(traj_points[0], traj_points[1], 'b-o')
-
             searchfile = open(sys.argv[2])
             lines = searchfile.readlines()
             res = np.array(eval(lines[0]))
             searchfile.close()
             controller = NN(res).controller
             trackers = []
-            for i in range(N_TEST_DIRECTIONS):
-                for traj in paths[N_TRAIN_TRAJ:]:
+            for i in range(N_TRAIN_DIRECTIONS):
+                for traj in paths:
                     traj = Trajectory(traj)
-                    trackers.append(Tracker(VELOCITY,i*2*pi/N_TEST_DIRECTIONS,traj,controller))
+                    trackers.append(Tracker(VELOCITY,i*2*pi/N_TRAIN_DIRECTIONS,traj,controller))
             traces = map(lambda x: x.run(), trackers)
             for i, trace in enumerate(traces):
-                plt.plot(trace[1], trace[2], '-', color=hsv_to_rgb(linspace(0, 1, len(traces))[i],1,1))
+                plt.plot(trace[1], trace[2], ':', color=hsv_to_rgb(linspace(0, 1, len(traces))[i],1,1))
+	    for path in paths:
+		plt.plot(*zip(*path))
             plt.show()
         else:
             print "file does not exist"